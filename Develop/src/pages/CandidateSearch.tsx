1c79550f6978ed78553b852ffad28f52ee1b766c
kurlee710 <josephatamaniuk@gmail.com>
2024-10-11 22:19:14 -0400

    created candidate search and fetchcandidate

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -3,6 +3,34 @@ import { searchGithubUsers } from "../api/API";
 import { Candidate } from "../interfaces/Candidate.interface";
 
 const CandidateSearch = () => {
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+  const [currentCandidateIndex, setCurrentCandidateIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [savedCandidates, setSavedCandidates] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    const fetchCandidates = async () => {
+      try {
+        setLoading(true);
+        const usernames = [
+          "octocat",
+          "torvalds",
+          "gaearon",
+          "paolanayala",
+          "Theon87",
+        ]; // You can make this dynamic
+        const userData = await searchGithubUsers(usernames);
+        setCandidates(userData);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCandidates();
+  }, []);
   return <h1>CandidateSearch</h1>;
 };
 
